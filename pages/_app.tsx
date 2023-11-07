8b5bb1613350cbf22cccb247c0112bd90deba35c
Narek <noah@logixsoftware.com>
2023-11-07 20:26:50 +0400

    work on meta tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -21,7 +21,6 @@ export default function App({ Component, pageProps }: AppProps) {
     <Layout>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <Component {...pageProps} />
     </Layout>
